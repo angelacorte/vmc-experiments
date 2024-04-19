7528a7a8e090dcfd978f3526d3460e7bb5058b7e
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-04-19 16:39:05 +0200

    feat: add distancebetween function

--- src/main/kotlin/it/unibo/collektive/lib/GradientCast.kt
+++ src/main/kotlin/it/unibo/collektive/lib/GradientCast.kt
@@ -3,7 +3,7 @@ package it.unibo.collektive.lib
 import it.unibo.alchemist.collektive.device.DistanceSensor
 import it.unibo.collektive.aggregate.api.Aggregate
 import it.unibo.collektive.aggregate.api.operators.share
-import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
+import it.unibo.collektive.field.Field.Companion.fold
 import it.unibo.collektive.field.min
 import it.unibo.collektive.field.plus
 import kotlin.Double.Companion.POSITIVE_INFINITY
@@ -28,3 +28,24 @@ fun <ID: Any> Aggregate<ID>.gradientCast(source: Boolean, initial: Double): Doub
 context(DistanceSensor)
 fun <ID: Any> Aggregate<ID>.distanceTo(source: Boolean): Double =
     gradientCast(source, if (source) 0.0 else POSITIVE_INFINITY)
+
+context(DistanceSensor)
+fun <ID: Any> Aggregate<ID>.distanceBetween(source: Boolean, destination: Boolean): Double =
+    broadcast(source, distanceTo(destination)) { it }
+
+context(DistanceSensor)
+fun <ID: Any> Aggregate<ID>.broadcast(source: Boolean, value: Double, accumulator: (Double) -> Double): Double =
+    share(POSITIVE_INFINITY to value) { field ->
+        val dist = distances()
+        when {
+            source -> 0.0 to value
+            else -> {
+                val resultField = dist.alignedMap(field) { distField, (currDist, value) ->
+                    distField + currDist to accumulator(value)
+                }
+                resultField.fold(POSITIVE_INFINITY to POSITIVE_INFINITY) { acc, value ->
+                    if (value.first < acc.first) value else acc
+                }
+            }
+        }
+    }.second
