866202e9d883ff26f76a4d30704abf30e70f6845
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-04-20 23:32:50 +0200

    feat: using bounded election

--- src/main/kotlin/it/unibo/collektive/lib/ConvergeAndSpread.kt
+++ src/main/kotlin/it/unibo/collektive/lib/ConvergeAndSpread.kt
@@ -3,18 +3,30 @@ package it.unibo.collektive.lib
 import it.unibo.alchemist.collektive.device.DistanceSensor
 import it.unibo.collektive.aggregate.api.Aggregate
 import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
-import it.unibo.collektive.field.Field.Companion.hood
-import kotlin.math.max
+import it.unibo.collektive.alchemist.device.sensors.RandomGenerator
+import it.unibo.collektive.field.Field
 
-context(Aggregate<Int>, DistanceSensor, EnvironmentVariables)
-fun convergeAndSpread(): Double {
-    val potential = distanceTo(localId == 0)
+context(DistanceSensor, EnvironmentVariables, RandomGenerator)
+fun Aggregate<Int>.convergeAndSpread(): Double {
+    val leader = boundedElection(localId, 15.0) //localId == 0 //
+    set("source", leader)
+    val potential = distanceTo(leader)
     set("potential", potential)
-    val localSuccess = max(0, 20 - neighboring(1).hood(0) { a, b -> a + b})
+//    val localSuccess = max(0, 20 - neighboring(1).hood(0) { a, b -> a + b})
+    val localSuccess = nextRandomDouble(0.0..20.0)
     set("localSuccess", localSuccess)
     val success = convergeCast(potential, localSuccess) { a, b -> a + b }
     set("success", success)
-    return spreadToChildren(potential, if (localId == 0) 200.0 else 0.0, success.toDouble()).also {
+    return spreadToChildren(potential, if (leader) 500.0 else 0.0, success).also {
         set("resource", it)
+        if (it >= 70) set("cloning", true)
     }
+}
+
+data class MyHopMetric(val step: Double = 1.0) : DistanceSensor {
+    override fun <ID : Any> Aggregate<ID>.distances(): Field<ID, Double> =
+        neighboring(step)
+            .mapWithId { id, value ->
+            if (id == localId) 0.0 else value + step
+        }
 }
