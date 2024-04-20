995c008b43e59c4fc8a9ca2c3651b1d3123b963c
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-04-20 22:57:35 +0200

    feat: working bounded election

---
+++ src/main/kotlin/it/unibo/collektive/lib/LeaderElection.kt
@@ -0,0 +1,39 @@
+package it.unibo.collektive.lib
+
+import it.unibo.alchemist.collektive.device.DistanceSensor
+import it.unibo.collektive.aggregate.api.Aggregate
+import it.unibo.collektive.aggregate.api.operators.share
+import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
+import it.unibo.collektive.field.Field
+import it.unibo.collektive.field.Field.Companion.fold
+
+context(DistanceSensor, EnvironmentVariables)
+fun <ID: Any, C : Comparable<C>> Aggregate<ID>.boundedElection(
+    localStrength: C,
+    radius: Double,
+): Boolean {
+    data class Candidacy<ID: Any>(val strength: C, val distance: Double, val leaderId: ID): Comparable<Candidacy<ID>> {
+        override fun compareTo(other: Candidacy<ID>): Int =
+            Comparator<Candidacy<ID>> { a, b -> a.strength.compareTo(b.strength) }
+                .thenBy { it.distance}
+                .thenBy {
+                    when(it.leaderId) {
+                        is Comparable<*> -> it.leaderId
+                        else -> 0
+                    }
+                }
+                .compare(this, other)
+    }
+    val local: Candidacy<ID> = Candidacy(localStrength, 0.0, localId)
+    return share(local) { candidates ->
+        val candidate = candidates
+            .alignedMap(distances()) { c, m -> Candidacy(c.strength,c.distance + m, c.leaderId) }
+        val field: Field<ID, Candidacy<ID>?> = candidate
+            .mapWithId { id, c -> c.takeUnless { id == localId || it.distance > radius } }
+        field.fold(local) { accumulator, newValue -> when {
+            newValue == null -> accumulator
+            else -> minOf(accumulator, newValue)
+        } }
+    }.leaderId == localId
+}
+
