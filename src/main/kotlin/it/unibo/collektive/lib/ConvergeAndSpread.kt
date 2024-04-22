e9c7cd1fbe1005bc4aa2192350c6282fcee0a50d
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-04-22 15:27:51 +0200

    feat: using environment variables for setting molecules

--- src/main/kotlin/it/unibo/collektive/lib/ConvergeAndSpread.kt
+++ src/main/kotlin/it/unibo/collektive/lib/ConvergeAndSpread.kt
@@ -2,34 +2,30 @@ package it.unibo.collektive.lib
 
 import it.unibo.alchemist.collektive.device.CollektiveDevice
 import it.unibo.alchemist.collektive.device.DistanceSensor
-import it.unibo.alchemist.model.Position
 import it.unibo.collektive.aggregate.api.Aggregate
-import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
 import it.unibo.collektive.alchemist.device.sensors.RandomGenerator
 import it.unibo.collektive.field.Field
 
 context(RandomGenerator, CollektiveDevice<*>)
 fun Aggregate<Int>.convergeAndSpread(): Double {
-    localId.set("ids $localId", id)
-    val leader = with(MyHopMetric()) {
-        boundedElection(localId, 12.0) //localId == 0 //
+    val (leader, potential) = with(MyHopMetric()) {
+        val l = boundedElection(localId, 12.0) //localId == 0 //
+        l to distanceTo(l)
     }
-    localId.set("source", leader)
-    val potential = with(MyHopMetric()) {
-        distanceTo(leader)
-    }
-    localId.set("potential", potential)
-//    val localSuccess = max(0, 20 - neighboring(1).hood(0) { a, b -> a + b})
+    set("source", leader)
+    set("potential", potential)
     val localSuccess = nextRandomDouble(0.0..20.0)
-    localId.set("localSuccess", localSuccess)
+//    val count = repeat(1.0) { a -> a + 1.0}
+//    val localSuccess = if (count < 400.0) nextRandomDouble(0.0..20.0) else get("localSuccess")
+    set("localSuccess", localSuccess)
     val success = convergeCast(potential, localSuccess) { a, b -> a + b }
-    localId.set("success", success)
+    set("success", success)
     return spreadToChildren(potential, if (leader) 500.0 else 0.0, success).also {
-        localId.set("resource", it)
-        if (it >= 400) {
+        set("resource", it)
+        if (it >= 200) {
             val cloned = repeat(1) { a -> a + 1 }
-            if(cloned in 10..14) {
-                localId.set("cloning", true)
+            if(cloned % 500 == 0) {
+                set("cloning", true)
             }
         }
     }
