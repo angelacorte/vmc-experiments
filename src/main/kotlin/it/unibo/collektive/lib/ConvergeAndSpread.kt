7136152a9e5a577a56a0d9e2ae85784a90ae4c8a
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-04-24 15:42:52 +0200

    feat: get variables from environment

--- src/main/kotlin/it/unibo/collektive/lib/ConvergeAndSpread.kt
+++ src/main/kotlin/it/unibo/collektive/lib/ConvergeAndSpread.kt
@@ -1,37 +1,36 @@
 package it.unibo.collektive.lib
 
-import it.unibo.alchemist.collektive.device.CollektiveDevice
 import it.unibo.alchemist.collektive.device.DistanceSensor
 import it.unibo.collektive.aggregate.api.Aggregate
-import it.unibo.collektive.alchemist.device.sensors.RandomGenerator
+import it.unibo.collektive.alchemist.device.sensors.EnvironmentLayer
+import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
 import it.unibo.collektive.field.Field
 
-context(RandomGenerator, CollektiveDevice<*>)
-fun Aggregate<Int>.convergeAndSpread(): Double {
-    val (leader, potential) = with(MyHopMetric()) {
-        val l = boundedElection(localId, 12.0) //localId == 0 //
-        l to distanceTo(l)
-    }
+context(EnvironmentVariables, EnvironmentLayer)
+fun Aggregate<Int>.convergeAndSpread(): Double = with(MyHopMetric()){
+    val leader = boundedElection(localId, getOrDefault("leaderRadius", 12.0)) //localId == 0 //
+    val potential = distanceTo(leader)
     set("source", leader)
     set("potential", potential)
-    val localSuccess = nextRandomDouble(0.0..20.0)
-//    val count = repeat(1.0) { a -> a + 1.0}
-//    val localSuccess = if (count < 400.0) nextRandomDouble(0.0..20.0) else get("localSuccess")
+    val localSuccess = getFromLayer<Double>("successSource")
     set("localSuccess", localSuccess)
     val success = convergeCast(potential, localSuccess) { a, b -> a + b }
     set("success", success)
-    return spreadToChildren(potential, if (leader) 500.0 else 0.0, success).also {
+    val condition = if (leader) {
+        getOrDefault("initialResource", 500.0)
+    } else 0.0
+    spreadToChildren(potential, condition, success).also {
         set("resource", it)
-        if (it >= 200) {
+        if (it >= getOrDefault("resourceTh", 200.0)) {
             val cloned = repeat(1) { a -> a + 1 }
-            if(cloned % 500 == 0) {
+            if(cloned % getOrDefault("cloningRatio", 500.0) == 0.0) {
                 set("cloning", true)
             }
         }
     }
 }
 
-data class MyHopMetric(val step: Double = 1.0) : DistanceSensor {
+data class MyHopMetric(val step: Double = 1.0): DistanceSensor {
     override fun <ID : Any> Aggregate<ID>.distances(): Field<ID, Double> =
         neighboring(step)
             .mapWithId { id, value ->
