9d4e7830cb505b39eecd7667ceab4dc5db2bf70f
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-05-02 09:48:35 +0200

    feat: use new sensors

--- src/main/kotlin/it/unibo/collektive/vmc/VMCSource.kt
+++ src/main/kotlin/it/unibo/collektive/vmc/VMCSource.kt
@@ -2,16 +2,20 @@ package it.unibo.collektive.vmc
 
 import it.unibo.alchemist.collektive.device.DistanceSensor
 import it.unibo.collektive.aggregate.api.Aggregate
+import it.unibo.collektive.alchemist.device.sensors.DeviceSpawn
 import it.unibo.collektive.alchemist.device.sensors.EnvironmentLayer
 import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
+import it.unibo.collektive.alchemist.device.sensors.LocationSensor
+import it.unibo.collektive.alchemist.device.sensors.SelfDestroy
+import it.unibo.collektive.lib.metrics.MyHopMetric
 import it.unibo.collektive.lib.chooseLeader
 import it.unibo.collektive.lib.convergeSuccess
 import it.unibo.collektive.lib.findPotential
 import it.unibo.collektive.lib.obtainLocalSuccess
 import it.unibo.collektive.lib.spreadResource
 
-context(EnvironmentVariables, EnvironmentLayer, DistanceSensor)
-fun Aggregate<Int>.vmcSource(): Double {
+context(EnvironmentVariables, EnvironmentLayer, DistanceSensor, DeviceSpawn, SelfDestroy, LocationSensor)
+fun Aggregate<Int>.vmcSource(): Double = with(MyHopMetric()){
     val isLeader = chooseLeader()
     val potential = findPotential(isLeader)
     val localSuccess = obtainLocalSuccess()
