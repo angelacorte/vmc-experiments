2e5edc849935a627cf0fc0b139888ce55aa98742
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-05-02 13:09:07 +0200

    feat: implement spawn based on childs number

--- src/main/kotlin/it/unibo/collektive/alchemist/device/sensors/impl/DeviceSpawner.kt
+++ src/main/kotlin/it/unibo/collektive/alchemist/device/sensors/impl/DeviceSpawner.kt
@@ -16,7 +16,7 @@ import kotlin.math.nextUp
 class DeviceSpawner<T, P: Position<P>>(
     private val ratio: Double,
     private val resourceUpperBound: Double,
-    private val maxChilds: Int? = null,
+    private val maxChilds: Int,
     private val environment: Environment<T, P>,
     override val node: Node<T>,
     private val randomGenerator: RandomGenerator,
@@ -24,8 +24,10 @@ class DeviceSpawner<T, P: Position<P>>(
     override fun cloneOnNewNode(node: Node<T>): NodeProperty<T> =
         DeviceSpawner(ratio, resourceUpperBound, maxChilds, environment, node, randomGenerator)
 
+    @Suppress("UNCHECKED_CAST")
     override fun <ID : Any> spawnIfNeeded(neighbors: Field<ID, Pair<Double, Double>>, spawn: Boolean) {
         val resourceMolecule = SimpleMolecule("resource")
+        val childsMolecule = SimpleMolecule("childs")
         if( spawn && node.contains(resourceMolecule) &&
             node.getConcentration(resourceMolecule) as Double >= resourceUpperBound ) {
             val nodePosition = environment.getPosition(node).coordinates
@@ -33,7 +35,7 @@ class DeviceSpawner<T, P: Position<P>>(
             val cloneOfThis = node.cloneNode(
                 environment.simulation.time + DoubleTime(randomGenerator.nextDouble(0.0.nextUp(), 0.1))
             )
-
+            if(cloneOfThis.contains(childsMolecule)) cloneOfThis.setConcentration(childsMolecule, 0.0 as T)
             val updatedPosition = environment.makePosition(nodePosition[0] + vector.first, nodePosition[1] + vector.second)
             environment.addNode(cloneOfThis, updatedPosition)
         }
@@ -42,9 +44,16 @@ class DeviceSpawner<T, P: Position<P>>(
     override fun <ID : Any> Aggregate<ID>.spawnWithRatio(): Boolean =
         repeat(1) {a -> a + 1} % ratio == 0.0
 
-
+    @Suppress("UNCHECKED_CAST")
     override fun <ID : Any> Aggregate<ID>.spawnWithChildsNumber(): Boolean {
-        TODO("Not yet implemented")
+        val childsMolecule = SimpleMolecule("childs")
+        if(node.contains(childsMolecule)) {
+            node.setConcentration(childsMolecule, (node.getConcentration(childsMolecule) as Double + 1.0) as T)
+        } else {
+            node.setConcentration(childsMolecule, 1.0 as T)
+        }
+        val childs = node.getConcentration(childsMolecule) as Double
+        return childs < maxChilds
     }
 
     private fun <ID: Any> Field<ID, Pair<Double, Double>>.averageDistanceVector(): Pair<Double, Double>{
