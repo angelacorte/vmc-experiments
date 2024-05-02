25acfc9d425c10d2989a5d42379eb085a522f83d
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-05-02 09:49:53 +0200

    feat: create device spawner sensor

---
+++ src/main/kotlin/it/unibo/collektive/alchemist/device/sensors/DeviceSpawn.kt
@@ -0,0 +1,12 @@
+package it.unibo.collektive.alchemist.device.sensors
+
+import it.unibo.collektive.aggregate.api.Aggregate
+import it.unibo.collektive.field.Field
+
+interface DeviceSpawn {
+    fun <ID : Any> spawnIfNeeded(neighbors: Field<ID, Pair<Double, Double>>, spawn: Boolean)
+
+    fun <ID : Any> Aggregate<ID>.spawnWithRatio(): Boolean
+
+    fun <ID : Any> Aggregate<ID>.spawnWithChildsNumber(): Boolean
+}
