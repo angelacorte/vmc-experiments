62f89523614698af4dbe9f143663de559a7f886b
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-05-02 16:27:53 +0200

    feat: add function to set the leader id

--- src/main/kotlin/it/unibo/collektive/alchemist/device/sensors/LeaderSensor.kt
+++ src/main/kotlin/it/unibo/collektive/alchemist/device/sensors/LeaderSensor.kt
@@ -1,7 +1,11 @@
 package it.unibo.collektive.alchemist.device.sensors
 
 interface LeaderSensor {
+    val leaderRadius: Double
+
     fun isLeader(): Boolean
 
     fun setLeader(leader: Boolean)
+
+    fun <ID: Any> setLeaderId(id: ID)
 }
--- src/main/kotlin/it/unibo/collektive/alchemist/device/sensors/impl/LeaderSensorProperty.kt
+++ src/main/kotlin/it/unibo/collektive/alchemist/device/sensors/impl/LeaderSensorProperty.kt
@@ -8,11 +8,12 @@ import it.unibo.alchemist.model.molecules.SimpleMolecule
 import it.unibo.collektive.alchemist.device.sensors.LeaderSensor
 
 class LeaderSensorProperty<T, P: Position<P>>(
+    override val leaderRadius: Double,
     private val environment: Environment<T, P>,
     override val node: Node<T>,
 ): LeaderSensor, NodeProperty<T> {
     override fun cloneOnNewNode(node: Node<T>): NodeProperty<T> =
-        LeaderSensorProperty(environment, node)
+        LeaderSensorProperty(leaderRadius, environment, node)
 
     override fun isLeader(): Boolean {
         val leaderMolecule = SimpleMolecule("leader")
@@ -23,4 +24,8 @@ class LeaderSensorProperty<T, P: Position<P>>(
     override fun setLeader(leader: Boolean) {
         node.setConcentration(SimpleMolecule("leader"), leader as T)
     }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <ID : Any> setLeaderId(id: ID) =
+        node.setConcentration(SimpleMolecule("leaderId"), id as T)
 }
